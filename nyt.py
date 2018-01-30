9635087bc8730c1604ac753ae5467bf556e50fcd
Yang Yang <yangyang@YangdeMacBook-Pro.local>
2018-01-30 17:55:45 -0500

    add nyt

---
+++ nyt.py
@@ -0,0 +1,21 @@
+
+from secrets import *
+import requests
+
+def get_stories(section):
+    baseurl = 'https://api.nytimes.com/svc/topstories/v2/'
+    extendedurl = baseurl + section + '.json'
+    params={'api-key': nyt_key}
+    return requests.get(extendedurl, params).json()
+
+def get_headlines(nyt_results_dict):
+    results = nyt_results_dict['results']
+    headlines = []
+    for r in results:
+        headlines.append(r['title'])
+    return headlines
+
+story_list_json = get_stories('science')
+headlines = get_headlines(story_list_json)
+for h in headlines:
+    print(h)
